ff16030875261f609613ab45ac319afaede81708
Trinity Christiana <trinitycterry@gmail.com>
2022-01-09 21:27:43 -0600

    ASGI is running server

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -9,6 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
-  "http": AsgiHandler(),
-  # Just HTTP for now. (We can add other protocols later.)
+    "http": AsgiHandler(),
+    # Just HTTP for now. (We can add other protocols later.)
 })
